3c7eed285f6a4925c7e58da2fae719b9770087df
Dustin Redmond <dustinkredmond@gmail.com>
2021-04-23 11:31:12 -0400

    Update copyright date

--- src/main/java/com/dustinredmond/fxalert/AlertBuilder.java
+++ src/main/java/com/dustinredmond/fxalert/AlertBuilder.java
@@ -1,7 +1,7 @@
 package com.dustinredmond.fxalert;
 
 /*
- *  Copyright 2020  Dustin K. Redmond
+ *  Copyright 2021  Dustin K. Redmond
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
